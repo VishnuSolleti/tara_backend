f79b001d480124399abe05ed9016e0de3ea1c38f
VishnuSolleti <taraintern3@tarafirst.com>
2025-07-05 22:07:47 +0530

    myDeploy_04

--- scripts/applicationstart.sh
+++ scripts/applicationstart.sh
@@ -6,7 +6,8 @@ cd /home/ubuntu/tara_dev_backend
 
 # In deployment scripts
 export DOCKER_IMAGE="sadanandtummuri/my_backend_images"
-export IMAGE_TAG=$(cat image_tag.txt)
+# export IMAGE_TAG=$(cat image_tag.txt)
+IMAGE_TAG=$(cat image_tag.txt | tr -d '\n')
 docker-compose up -d
 
 echo "[ApplicationStart] Running DB migrations..."
