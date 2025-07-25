493fe388cb60d63889d8fe315a283dac52ef3f0b
VishnuSolleti <taraintern3@tarafirst.com>
2025-07-25 13:23:49 +0530

    testing single_shared02

--- usermanagement/views.py
+++ usermanagement/views.py
@@ -434,7 +434,7 @@ def delete_module_permission(request, permission_id):
 @permission_classes([AllowAny])
 def happy_coder(request):
     """Return a simple success message."""
-    return Response({"message": "Happy Coder, blue is successfull"},
+    return Response({"message": "Happy Coder, blue is successfull for single instance"},
                     status=status.HTTP_200_OK)
 
 
